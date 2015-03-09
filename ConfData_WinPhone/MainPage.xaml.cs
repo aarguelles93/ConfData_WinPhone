81f2e1a3abd35c20c88eaa2bb3cacaea129fa2a2
aarguelles <aarguelles@uninorte.edu.co>
2015-03-09 00:37:41 -0500

    Preference manager done!

---
+++ ConfData_WinPhone/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.Windows.Navigation;
+using System.IO.IsolatedStorage;
+
+namespace ConfData_WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings preferencias;
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            preferencias = IsolatedStorageSettings.ApplicationSettings;
+        
+            if (preferencias.Count <= 0)
+            {
+                preferencias["Nombre"] = "Usuario";
+                preferencias["Edad"] = "";
+                preferencias["Genero"] = "";
+            }
+        
+        }
+
+        //Cuando va a ir a la ventana de preferencias
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+          
+        }
+
+        //Cuando regresa de la ventana de preferencias
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            textBoxNombreDisp.Text = preferencias["Nombre"].ToString();
+            textBoxEdadDisp.Text = preferencias["Edad"].ToString();
+            textBoxGenDisp.Text = preferencias["Genero"].ToString();
+        }
+
+        private void GoToSettingsPage(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Preferencias.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ ConfData_WinPhone/Preferencias.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+
+namespace ConfData_WinPhone
+{
+    public partial class Preferencias : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings preferencias;
+
+        public Preferencias()
+        {
+            InitializeComponent();
+            preferencias = IsolatedStorageSettings.ApplicationSettings;
+        }
+
+        private void guardarPreferencias(object sender, RoutedEventArgs e)
+        {
+            preferencias["Nombre"] = textBoxNombre.Text.ToString();
+            preferencias["Edad"] = textBoxEdad.Text.ToString();
+            int ind = listBoxGenero.SelectedIndex;
+            ListBoxItem temp = (ListBoxItem) listBoxGenero.SelectedItem;
+            String gen="";
+            if (temp != null)
+            {
+                gen = temp.Content.ToString();
+            }
+            preferencias["Genero"] = gen;
+            //Notifico que los datos han sido guardados
+            MessageBox.Show("Los datos han sido salvados!");
+            
+        }
+
+        //Al abrir la página de preferencias
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            textBoxNombre.Text = preferencias["Nombre"].ToString();
+            textBoxEdad.Text = preferencias["Edad"].ToString();
+            if (preferencias["Genero"].ToString().CompareTo("Masculino") == 0)
+            {
+                listBoxGenero.SelectedIndex = 0;
+            }
+            else if (preferencias["Genero"].ToString().CompareTo("Femenino") == 0)
+            {
+                listBoxGenero.SelectedIndex = 1;
+            }
+            else
+                listBoxGenero.SelectedIndex = -1;
+           
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
